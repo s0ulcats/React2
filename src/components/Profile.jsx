e9cde8ee5203f8e2963af454b61eef8057d0b7ed
s0ulcats <babich.danya30@gmail.com>
2024-08-11 15:19:38 +0300

    lesson13

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <img src='https://c4.wallpaperflare.com/wallpaper/842/306/567/cool-pictures-of-mountains-1920x1080-wallpaper-preview.jpg' />
+        <div>
+            ava + description
+        </div>
+        <div>
+            my posts
+            <div>
+                new post
+            </div>
+            <div>
+                post1
+            </div>
+            <div>
+                post2
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
